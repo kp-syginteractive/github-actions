b001a41902faaa0e512110abcd289100a2b9b9fd
Kazim P <kazim.parkar@syginteractive.com.au>
2020-06-11 16:26:34 +1000

    Testing modifications 14

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
@@ -14,5 +15,9 @@ class ExampleTest extends TestCase
     {
         $this->artisan('create-user');
         $this->assertEquals(1, User::count());
+
+        $response = Http::get('https://www.brooksrunning.com.au/payment');
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals('/cart', $response->effectiveUri()->getPath());
     }
 }
